567e7ef10c6a6f7c8ecc01948196bcc3436fbe23
km3979 <108575443+km3979@users.noreply.github.com>
2023-10-05 01:33:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const http = require("http");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
+// Tăng giới hạn kích thước payload lên 50MB
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 const cors = require("cors");
 app.use(cors());
 
