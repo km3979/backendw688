9538d6bdb4f4f3dbfb9fab76120cf5881d7cded5
km3979 <108575443+km3979@users.noreply.github.com>
2023-10-05 12:42:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const http = require("http");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+
+// Sử dụng bodyParser để có thể đọc dữ liệu gửi đến từ client
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Thông tin đăng nhập cơ sở dữ liệu WordPress
 const dbConfig = {
@@ -23,26 +28,32 @@ connection.connect((err) => {
   }
 });
 
-// Xử lý REST API để lấy dữ liệu từ WordPress (ví dụ)
-app.get("/getWordPressData", async (req, res) => {
+// Xử lý REST API để lấy danh sách bài viết từ WordPress
+app.get("/getWordPressPosts", async (req, res) => {
   try {
-    // Thực hiện truy vấn cần thiết để lấy dữ liệu từ WordPress
-    // Ví dụ: const wordpressData = await getWordPressData();
-    // Gửi dữ liệu về client
-    res.json({ success: true, data: wordpressData });
+    // Thực hiện truy vấn cần thiết để lấy danh sách bài viết từ WordPress
+    const query = "SELECT * FROM wp_posts WHERE post_type = 'post' AND post_status = 'publish'";
+    const [rows, fields] = await connection.promise().query(query);
+
+    // Gửi danh sách bài viết về client
+    res.json({ success: true, data: rows });
   } catch (error) {
-    console.error("Lỗi khi lấy dữ liệu WordPress:", error);
-    res.json({ success: false, error: "Lỗi khi lấy dữ liệu WordPress" });
+    console.error("Lỗi khi lấy danh sách bài viết từ WordPress:", error);
+    res.json({ success: false, error: "Lỗi khi lấy danh sách bài viết từ WordPress" });
   }
 });
 
-// Xử lý REST API để tạo bài đăng mới trong cơ sở dữ liệu của bạn
-app.post("/createBlogPost", async (req, res) => {
+// Xử lý REST API để tạo bài đăng mới trong cơ sở dữ liệu WordPress
+app.post("/createWordPressPost", async (req, res) => {
   try {
-    const blogPostData = req.body;
-    // Thực hiện xử lý để tạo bài đăng mới trong cơ sở dữ liệu của bạn
-    // Ví dụ: const result = await createBlogPost(blogPostData);
-    res.json({ success: true, message: "Bài viết đã được tạo thành công" });
+    const { title, content, author } = req.body;
+
+    // Thực hiện truy vấn cần thiết để tạo bài đăng mới trong WordPress
+    const query = "INSERT INTO wp_posts (post_title, post_content, post_author, post_type, post_status) VALUES (?, ?, ?, 'post', 'publish')";
+    const [result] = await connection.promise().query(query, [title, content, author]);
+
+    // Gửi thông báo về client
+    res.json({ success: true, message: "Bài viết đã được tạo thành công", postId: result.insertId });
   } catch (error) {
     console.error("Lỗi khi tạo bài viết mới:", error);
     res.json({ success: false, error: "Lỗi khi tạo bài viết mới" });
